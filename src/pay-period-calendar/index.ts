093b7ae5ad6835130de66403bf18585896def4ad
martin-the-geek <martin2great@yahoo.com>
2016-10-19 19:52:27 -0400

    ensure models in barrels do not cause undefined references

--- src/pay-period-calendar/index.ts
+++ src/pay-period-calendar/index.ts
@@ -1,5 +1,5 @@
-﻿export * from "./pay-period-month.model";
-export * from "./pay-period.model";
+﻿export { PayPeriodMonth } from "./pay-period-month.model";
+export { PayPeriod } from "./pay-period.model";
 
 export * from "./pay-period-calendar.service";
 export * from "./pay-period-calendar.component";
