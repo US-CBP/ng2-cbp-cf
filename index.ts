c1adcea6e127fb0f2fcfa516fc473d4432d24687
martin-the-geek <martin2great@yahoo.com>
2017-08-29 15:12:34 -0400

    remove cbp-theme

--- index.ts
+++ index.ts
@@ -1,11 +1,4 @@
-﻿import 'cbp-theme/dist/cbp-theme.css';
-import 'cbp-theme/dist/jquery.js';
-import 'cbp-theme/dist/inputmask.js';
-// for now I need it for the header bootstrap to work
-// I will remove it soon when I have bootstrap menu component
-// who can be used in multi places
-import 'cbp-theme/dist/cbp-theme.js';
-import 'hammerjs';
+﻿import 'hammerjs';
 
 import 'material-design-icons/iconfont/material-icons.css';
 import './theme/cbp-theme.scss';
