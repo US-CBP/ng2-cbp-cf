71b02ae201af0f049880858dfafd6cabdd71b919
martin-the-geek <martin2great@yahoo.com>
2017-09-01 11:38:45 -0400

    upgrade libraries

--- src/header/toolbar/toolbar.module.ts
+++ src/header/toolbar/toolbar.module.ts
@@ -1,10 +1,10 @@
-﻿import { CommonModule }                     from '@angular/common';
+﻿import { PortalModule }                     from "@angular/cdk/portal";
+import { CommonModule }                     from '@angular/common';
 import { NgModule }                         from '@angular/core';
 import {
     MdButtonModule,
     MdIconModule,
     MdToolbarModule,
-    PortalModule,
 }                                           from '@angular/material';
 import { RouterModule }                     from '@angular/router';
 
@@ -12,8 +12,6 @@ import { BadgeModule }                      from '../../badge';
 
 import { LeftActionComponent }              from './left-action.component';
 import { SideNavComponent }                 from './side-nav.component';
-import { ToolbarHostDirective }             from './toolbar-host.directive';
-import { ToolbarTemplatePortalDirective }   from './toolbar-template-portal.directive';
 import { ToolbarComponent }                 from './toolbar.component';
 import { toolbarServiceProvider }           from './toolbar.service';
 
@@ -30,14 +28,11 @@ import { toolbarServiceProvider }           from './toolbar.service';
     exports: [
         SideNavComponent,
         ToolbarComponent,
-        ToolbarTemplatePortalDirective,
     ],
     declarations: [
         LeftActionComponent,
         SideNavComponent,
         ToolbarComponent,
-        ToolbarHostDirective,
-        ToolbarTemplatePortalDirective,
     ],
     providers: [
         toolbarServiceProvider,
